b33cdf189bd6453822d7f0af1616de370b8d10c4
Neutron-Pro <neutron@neutronstars.fr>
2023-08-20 23:40:52 +0200

    Ajout des converter

--- src/main/java/fr/aimcvent/bukkit/api/command/ConverterType.java
+++ src/main/java/fr/aimcvent/bukkit/api/command/ConverterType.java
@@ -9,5 +9,8 @@ public class ConverterType {
     public static final String FLOAT = "float";
     public static final String DOUBLE = "double";
     public static final String PLAYER = "player";
+    public static final String OFFLINE_PLAYER = "offline_player";
     public static final String MATERIAL = "material";
+    public static final String TIME = "time";
+    public static final String ARRAY = "array";
 }
