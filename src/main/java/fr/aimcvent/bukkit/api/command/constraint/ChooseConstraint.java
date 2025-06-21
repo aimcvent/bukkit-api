6f324aa0eb4a1f21ed4e7f641d72e78af9b28722
Neutron-Pro <neutron@neutronstars.fr>
2025-06-21 17:42:08 +0200

    Ajout d'un constructeur de Collection<String>

--- src/main/java/fr/aimcvent/bukkit/api/command/constraint/ChooseConstraint.java
+++ src/main/java/fr/aimcvent/bukkit/api/command/constraint/ChooseConstraint.java
@@ -3,15 +3,17 @@ package fr.aimcvent.bukkit.api.command.constraint;
 import fr.aimcvent.bukkit.api.command.ArgumentConstraint;
 import org.bukkit.command.CommandSender;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class ChooseConstraint implements ArgumentConstraint<String> {
     private final Set<String> values = new HashSet<>();
 
     public ChooseConstraint(String... values) {
-        this.values.addAll(Arrays.asList(values));
+        this(Arrays.asList(values));
+    }
+
+    public ChooseConstraint(Collection<String> values) {
+        this.values.addAll(values);
     }
 
     @Override
